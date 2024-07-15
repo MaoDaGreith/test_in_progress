3dac5d992f17d3ee1063fed91ea26c3f7e848f85
Traian Lupanciuc <traianlupanciuc@Traians-MacBook-Pro.local>
2024-07-15 20:24:31 +0300

    Adding the solution

--- main.go
+++ main.go
@@ -1,29 +1,63 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"arkis_test/database"
 	"arkis_test/processor"
 	"arkis_test/queue"
-	"context"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	ctx := context.Background()
+	ch := make(chan os.Signal)
+	defer close(ch)
+	signal.Notify(ch, syscall.SIGINT)
 
-	inputQueue, err := queue.New(os.Getenv("RABBITMQ_URL"), "input-A")
+	ctxA := context.Background()
+	ctxB := context.Background()
+
+	inputQueueA, err := queue.New(os.Getenv("RABBITMQ_URL"), "input-A")
 	if err != nil {
-		log.WithError(err).Panic("Cannot create input queue")
+		log.WithError(err).Panic("Cannot create input queue A")
 	}
 
-	outputQueue, err := queue.New(os.Getenv("RABBITMQ_URL"), "output-A")
+	outputQueueA, err := queue.New(os.Getenv("RABBITMQ_URL"), "output-A")
 	if err != nil {
-		log.WithError(err).Panic("Cannot create output queue")
+		log.WithError(err).Panic("Cannot create output queue A")
+	}
+
+	inputQueueB, err := queue.New(os.Getenv("RABBITMQ_URL"), "input-B")
+	if err != nil {
+		log.WithError(err).Panic("Cannot create input queue B")
+	}
+
+	outputQueueB, err := queue.New(os.Getenv("RABBITMQ_URL"), "output-B")
+	if err != nil {
+		log.WithError(err).Panic("Cannot create output queue B")
 	}
 
 	log.Info("Application is ready to run")
 
-	processor.New(inputQueue, outputQueue, database.D{}).Run(ctx)
+	go func() {
+		if err := processor.New(inputQueueA, outputQueueA, database.D{}).Run(ctxA); err != nil {
+			log.WithError(err).Fatal("Error running processor A")
+		}
+	}()
+
+	go func() {
+		if err := processor.New(inputQueueB, outputQueueB, database.D{}).Run(ctxB); err != nil {
+			log.WithError(err).Fatal("Error running processor B")
+		}
+	}()
+
+	select {
+	case <-ch:
+		log.Info("Received interrupt signal, shutting down...")
+		os.Exit(0) // Graceful exit
+	}
 }
